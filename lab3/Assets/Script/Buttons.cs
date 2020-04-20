9f1141da4d40963526f1ce2d07d25af70444eff2
lauratamath <laurylevi@hotmail.com>
2020-04-20 00:02:04 -0600

    no me deja agregar funciones

---
+++ lab3/Assets/Script/Buttons.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
+
+public class Buttons : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    private bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TogglePause()
+    {
+        if (pauseMenu)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            active = !active;
+            Time.timeScale = active ? 0.0f : 1.0f;
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
